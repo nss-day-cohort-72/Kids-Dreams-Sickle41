4d83c31b76e08f3d8527942c21d979dda8837b28
Sickle41 <sickle41@gmail.com>
2024-08-22 10:02:06 -0500

    last commit

--- scripts/CelebrityList.js
+++ scripts/CelebrityList.js
@@ -5,11 +5,11 @@ const celebrities = getCelebrities()
 export const Celebrities = () => {
     let html = "<ol>"
 
-    for (const star of celebrities) {
+    for (const celebrity of celebrities) {
         html += `<li 
                     data-id="${celebrity.id}" 
                     data-type="celebrity"
-                    data-sport="${celebrty.sport}"
+                    data-sport="${celebrity.sport}"
                     id="star--${celebrity.id}">
                     ${celebrity.name}
                 </li>`
@@ -18,3 +18,19 @@ export const Celebrities = () => {
     html += "</ol>"
     return html
 }
+
+document.addEventListener(
+    "click",
+    (theClickEvent) => {
+        const whatWasClickedOn = theClickEvent.target
+        
+        const celebrityId = whatWasClickedOn.dataset.id
+        if(whatWasClickedOn.dataset.type === "celebrity"){
+            for (const celebrity of celebrities) {
+                if (celebrity.id === parseInt(celebrityId)) {
+                window.alert(`${celebrity.name} is a ${celebrity.sport} star`);
+                }
+            }
+        }
+    }
+)
--- scripts/Kids.js
+++ scripts/Kids.js
@@ -6,10 +6,25 @@ export const Kids = () => {
     let html = "<ol>"
 
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child,name}</li>`
+        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child.name}</li>`
     }
 
     html += "</ol>"
     return html
 }
 
+document.addEventListener(
+    "click",
+    (theClickEvent) => {
+        const whatWasClickedOn = theClickEvent.target
+        
+        const kidId = whatWasClickedOn.dataset.id
+        if (whatWasClickedOn.dataset.type === "child"){
+            for (const kid of children) {
+                if (kid.id === parseInt(kidId)) {
+                    window.alert(`${kid.name}'s wish is ${kid.wish}`);
+                }
+            }
+        }
+    }
+)
